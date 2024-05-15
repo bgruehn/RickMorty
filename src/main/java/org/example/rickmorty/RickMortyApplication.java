8708041ac21e726a1d92b0a11d1627384014c642
bgruehn <benjamin.gruehn@gmail.com>
2024-05-15 11:33:13 +0200

    Initial commit

---
+++ src/main/java/org/example/rickmorty/RickMortyApplication.java
@@ -0,0 +1,13 @@
+package org.example.rickmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickMortyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/rickmorty/RickMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rickmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
